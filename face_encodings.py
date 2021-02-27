93c27a496cec49f07b3d6c63503cc86a93b6e81c
Radmir Sultamuratov <sultamuratov.r@gmail.com>
2021-02-27 16:17:16 -0600

    commit

--- face_encodings.py
+++ face_encodings.py
@@ -2,15 +2,14 @@ import face_recognition as fr
 import os
 import numpy as np
 
-dataset = 'test'
+dataset = 'train'
 
-txt = open("test-defected.txt", 'w')
-
+txt = open("train-defected.txt", 'w')
+total_defected = 0
 for i in range(1, 101):
     defected = []
     age = f'{i:03}'
     for file in os.listdir('/'.join((dataset, age))):
-
         image = fr.load_image_file('/'.join((dataset, age, file)))
         face_encodings = fr.face_encodings(image)
         if len(face_encodings) == 1:
@@ -18,7 +17,8 @@ for i in range(1, 101):
             np.save(path, np.array(face_encodings[0]))
         else:
             defected.append(file)
-
-    txt.write(f"{len(defected)} defected files in {i:03}: {','.join(defected)}\n")
-
+    d = len(defected)
+    total_defected += d
+    txt.write(f"{d} defected files in {i:03}: {','.join(defected)}\n")
+txt.write(f'Total defected is {total_defected}.')
 txt.close()
