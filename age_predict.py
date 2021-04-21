b05d52619254654121ad55eb8f9d584ef8424745
Radmir Sultamuratov <sultamuratov.r@gmail.com>
2021-04-21 06:19:41 -0500

    Added svm and reorganized bog/small data npy files

--- age_predict.py
+++ age_predict.py
@@ -1,14 +1,16 @@
 import face_recognition as fr
+import os
 import numpy as np
 
 
 def predict_LR(path):
     image = fr.load_image_file(path)
     face_encodings = np.array(fr.face_encodings(image)[0])
-    coeff = np.load("LinearRegCoeff.npy")
-    intercept = np.load("LinearRegInter.npy")
+    coeff = np.load("StupidLRcoef.npy")
+    intercept = np.load("StupidLRintercept.npy")
     return int(intercept + np.dot(face_encodings, coeff))
 
 
 if __name__ == "__main__":
-    print((predict_LR('example.jpg')))
+    for photo in os.listdir("examples"):
+        print(photo, predict_LR(f'examples/{photo}'))
